b2b2b23ccfb45d4e99796dbe53d1a5e484f8b6ea
4ertaile <vf451xx@gmail.com>
2024-09-22 23:20:53 +0300

    add battery sim/new holdRegisters

--- Invertor sim/Simulator.Designer.cs
+++ Invertor sim/Simulator.Designer.cs
@@ -13,6 +13,9 @@
         private System.Windows.Forms.Button buttonSyncTime;
         private System.Windows.Forms.TextBox textBoxSetTime;
         private System.Windows.Forms.Button buttonConfirmTime;
+        private System.Windows.Forms.ComboBox comboBoxBattery;
+        private System.Windows.Forms.NumericUpDown numericUpDownBatteryBlocks;
+        private System.Windows.Forms.ListBox listBoxBatteryInfo; // Заміна тут
 
         protected override void Dispose(bool disposing)
         {
@@ -35,15 +38,23 @@
             this.buttonSyncTime = new System.Windows.Forms.Button();
             this.textBoxSetTime = new System.Windows.Forms.TextBox();
             this.buttonConfirmTime = new System.Windows.Forms.Button();
+            this.comboBoxBattery = new System.Windows.Forms.ComboBox();
+            this.numericUpDownBatteryBlocks = new System.Windows.Forms.NumericUpDown();
+            this.listBoxBatteryInfo = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.battaryPowerInput = new System.Windows.Forms.TextBox();
+            this.BatteryPower = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_Error = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBatteryBlocks)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(736, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.Size = new System.Drawing.Size(93, 24);
             this.button1.TabIndex = 0;
             this.button1.Text = "Data generator";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,9 +76,9 @@
             // 
             // labelStatus
             // 
-            this.labelStatus.Location = new System.Drawing.Point(280, 20);
+            this.labelStatus.Location = new System.Drawing.Point(280, 24);
             this.labelStatus.Name = "labelStatus";
-            this.labelStatus.Size = new System.Drawing.Size(200, 28);
+            this.labelStatus.Size = new System.Drawing.Size(113, 21);
             this.labelStatus.TabIndex = 3;
             this.labelStatus.Text = "Status: Disconnected";
             // 
@@ -75,19 +86,19 @@
             // 
             this.listBoxReceivedCommands.Location = new System.Drawing.Point(20, 60);
             this.listBoxReceivedCommands.Name = "listBoxReceivedCommands";
-            this.listBoxReceivedCommands.Size = new System.Drawing.Size(400, 134);
+            this.listBoxReceivedCommands.Size = new System.Drawing.Size(400, 121);
             this.listBoxReceivedCommands.TabIndex = 4;
             // 
             // listBoxSentCommands
             // 
             this.listBoxSentCommands.Location = new System.Drawing.Point(20, 220);
             this.listBoxSentCommands.Name = "listBoxSentCommands";
-            this.listBoxSentCommands.Size = new System.Drawing.Size(400, 134);
+            this.listBoxSentCommands.Size = new System.Drawing.Size(400, 121);
             this.listBoxSentCommands.TabIndex = 5;
             // 
             // labelCurrentTime
             // 
-            this.labelCurrentTime.Location = new System.Drawing.Point(431, 7);
+            this.labelCurrentTime.Location = new System.Drawing.Point(422, 2);
             this.labelCurrentTime.Name = "labelCurrentTime";
             this.labelCurrentTime.Size = new System.Drawing.Size(49, 21);
             this.labelCurrentTime.TabIndex = 6;
@@ -95,9 +106,9 @@
             // 
             // buttonSyncTime
             // 
-            this.buttonSyncTime.Location = new System.Drawing.Point(415, 26);
+            this.buttonSyncTime.Location = new System.Drawing.Point(414, 20);
             this.buttonSyncTime.Name = "buttonSyncTime";
-            this.buttonSyncTime.Size = new System.Drawing.Size(84, 27);
+            this.buttonSyncTime.Size = new System.Drawing.Size(66, 25);
             this.buttonSyncTime.TabIndex = 7;
             this.buttonSyncTime.Text = "Sync Time";
             this.buttonSyncTime.UseVisualStyleBackColor = true;
@@ -105,7 +116,7 @@
             // 
             // textBoxSetTime
             // 
-            this.textBoxSetTime.Location = new System.Drawing.Point(498, 4);
+            this.textBoxSetTime.Location = new System.Drawing.Point(481, 1);
             this.textBoxSetTime.Name = "textBoxSetTime";
             this.textBoxSetTime.Size = new System.Drawing.Size(71, 20);
             this.textBoxSetTime.TabIndex = 8;
@@ -114,35 +125,101 @@
             // 
             // buttonConfirmTime
             // 
-            this.buttonConfirmTime.Location = new System.Drawing.Point(575, 2);
+            this.buttonConfirmTime.Location = new System.Drawing.Point(550, 0);
             this.buttonConfirmTime.Name = "buttonConfirmTime";
-            this.buttonConfirmTime.Size = new System.Drawing.Size(29, 24);
+            this.buttonConfirmTime.Size = new System.Drawing.Size(31, 22);
             this.buttonConfirmTime.TabIndex = 9;
             this.buttonConfirmTime.Text = "<--";
             this.buttonConfirmTime.UseVisualStyleBackColor = true;
             this.buttonConfirmTime.Click += new System.EventHandler(this.buttonConfirmTime_Click);
             // 
+            // comboBoxBattery
+            // 
+            this.comboBoxBattery.FormattingEnabled = true;
+            this.comboBoxBattery.Location = new System.Drawing.Point(20, 360);
+            this.comboBoxBattery.Name = "comboBoxBattery";
+            this.comboBoxBattery.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxBattery.TabIndex = 10;
+            // 
+            // numericUpDownBatteryBlocks
+            // 
+            this.numericUpDownBatteryBlocks.Location = new System.Drawing.Point(180, 360);
+            this.numericUpDownBatteryBlocks.Name = "numericUpDownBatteryBlocks";
+            this.numericUpDownBatteryBlocks.ReadOnly = true;
+            this.numericUpDownBatteryBlocks.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownBatteryBlocks.TabIndex = 11;
+            this.numericUpDownBatteryBlocks.ValueChanged += new System.EventHandler(this.numericUpDownBatteryBlocks_ValueChanged);
+            // 
+            // listBoxBatteryInfo
+            // 
+            this.listBoxBatteryInfo.Location = new System.Drawing.Point(20, 390);
+            this.listBoxBatteryInfo.Name = "listBoxBatteryInfo";
+            this.listBoxBatteryInfo.Size = new System.Drawing.Size(400, 147);
+            this.listBoxBatteryInfo.TabIndex = 12;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 7);
+            this.label1.Location = new System.Drawing.Point(17, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
-            this.label1.TabIndex = 10;
+            this.label1.TabIndex = 13;
             this.label1.Text = "Port";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(147, 7);
+            this.label2.Location = new System.Drawing.Point(147, 4);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 11;
+            this.label2.TabIndex = 14;
             this.label2.Text = "Baud rate";
+            // 
+            // battaryPowerInput
+            // 
+            this.battaryPowerInput.Location = new System.Drawing.Point(115, 543);
+            this.battaryPowerInput.Name = "battaryPowerInput";
+            this.battaryPowerInput.Size = new System.Drawing.Size(100, 20);
+            this.battaryPowerInput.TabIndex = 15;
+            // 
+            // BatteryPower
+            // 
+            this.BatteryPower.Location = new System.Drawing.Point(224, 541);
+            this.BatteryPower.Name = "BatteryPower";
+            this.BatteryPower.Size = new System.Drawing.Size(46, 23);
+            this.BatteryPower.TabIndex = 16;
+            this.BatteryPower.Text = "Set";
+            this.BatteryPower.UseVisualStyleBackColor = true;
+            this.BatteryPower.Click += new System.EventHandler(this.BatteryPower_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 548);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Set Battery Power";
+            // 
+            // label_Error
+            // 
+            this.label_Error.AutoSize = true;
+            this.label_Error.Location = new System.Drawing.Point(280, 546);
+            this.label_Error.Name = "label_Error";
+            this.label_Error.Size = new System.Drawing.Size(61, 13);
+            this.label_Error.TabIndex = 18;
+            this.label_Error.Text = "Label_Error";
             // 
             // Simulator
             // 
-            this.ClientSize = new System.Drawing.Size(839, 457);
+            this.ClientSize = new System.Drawing.Size(839, 644);
+            this.Controls.Add(this.label_Error);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BatteryPower);
+            this.Controls.Add(this.battaryPowerInput);
+            this.Controls.Add(this.listBoxBatteryInfo);
+            this.Controls.Add(this.numericUpDownBatteryBlocks);
+            this.Controls.Add(this.comboBoxBattery);
             this.Controls.Add(this.textBoxSetTime);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -157,6 +234,7 @@
             this.Controls.Add(this.buttonConfirmTime);
             this.Name = "Simulator";
             this.Text = "Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBatteryBlocks)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,5 +242,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox battaryPowerInput;
+        private System.Windows.Forms.Button BatteryPower;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_Error;
     }
 }
--- Invertor sim/Simulator.cs
+++ Invertor sim/Simulator.cs
@@ -21,11 +21,30 @@ namespace Invertor_sim
         private IModbusFactory modbusFactory;
         private InverterRegister[] registers;
         private DateTime currentTime;
+        private Battery selectedBattery;
 
+        private Battery[] batteries = new Battery[]
+        {
+            new Battery("No battery", 0, 40, 60, 0),
+            new Battery("Pylontech US5000", 100, 54, 42, 80),
+            new Battery("Deye BOS-GM5.1", 100, 57, 46, 50)
+        };
         public class InverterRegister
         {
             public ushort RegisterNumber { get; set; }
-            public float Value { get; set; }
+
+            private float _value;
+            public float Value
+            {
+                get => _value;
+                set
+                {
+                    if ((value >= MinValue) && (value <= MaxValue))
+                    {
+                        _value = value;
+                    }
+                }
+            }
             public string Unit { get; set; }
             public float MinValue { get; set; }
             public float MaxValue { get; set; }
@@ -46,7 +65,23 @@ namespace Invertor_sim
                 return (ushort)(Value * 100);
             }
         }
+        public class Battery
+        {
+            public string Name { get; set; }
+            public float NominalCapacity { get; set; } // Ah
+            public float MaxVoltage { get; set; } // V
+            public float MinVoltage { get; set; } // V
+            public float NominalChargeDischargeCurrent { get; set; } // A
 
+            public Battery(string name, float capacity, float maxV, float minV, float chargeCurrent)
+            {
+                Name = name;
+                NominalCapacity = capacity;
+                MaxVoltage = maxV;
+                MinVoltage = minV;
+                NominalChargeDischargeCurrent = chargeCurrent;
+            }
+        }
         public Simulator(MainForm main)
         {
             InitializeComponent();
@@ -56,21 +91,31 @@ namespace Invertor_sim
 
             registers = new InverterRegister[]
             {
-                new InverterRegister(0, 25.6f, "V", 0, 100, "Battery Voltage"),
-                new InverterRegister(1, 80.0f, "%", 0, 100, "State of Charge (SOC)")
+                new InverterRegister(0, 0f, "V", 0, 62, "Battery Voltage"),
+                new InverterRegister(1, 0f, "%", 0, 100, "State of Charge (SOC)"),
+                new InverterRegister(2, 1, "Blocks", 0, 50, "Battery Blocks"),
+                new InverterRegister(3, 0, "Ah", 0, 500, "Nominal Capacity"),
+                new InverterRegister(4, 54, "V", 40, 60, "Max Voltage"),
+                new InverterRegister(5, 42, "V", 40, 60, "Min Voltage"),
+                new InverterRegister(6, 0, "A", 0, 135, "Nominal Charge current"),
+                new InverterRegister(7, 0, "A", 0, 190, "Nominal discharge current"),
+                new InverterRegister(8, 0, "W", 0, 450400, "Max Power Capacity"),
+                new InverterRegister(9, 0, "W", 0, 0, "Battery Power"),
+                new InverterRegister(10, 0, "Text", 0, 11, "Register OverLimitError"),
+                new InverterRegister(11, 0, "Error", 0, 10, "Error"),
             };
-
+            UpdateRegisterDisplay();
+            //timer
             portCheckTimer = new System.Windows.Forms.Timer();
             portCheckTimer.Interval = 5000;
             portCheckTimer.Tick += new EventHandler(CheckAvailablePorts);
             portCheckTimer.Start();
-
+            //ports
             previousPorts = SerialPort.GetPortNames();
             comboBoxPorts.Items.Add("Port Not Selected");
             comboBoxPorts.Items.AddRange(previousPorts);
-
-            comboBoxBaudRate.Items.Add("9600");
-            comboBoxBaudRate.Items.Add("115200");
+            //BaudRate
+            comboBoxBaudRate.Items.AddRange(new object[] { "9600", "19200", "38400", "57600", "115200" });
             comboBoxBaudRate.SelectedIndex = 0;
 
             comboBoxPorts.SelectedIndexChanged += ComboBoxPorts_SelectedIndexChanged;
@@ -80,12 +125,42 @@ namespace Invertor_sim
             clockTimer.Interval = 1000; // Update every second
             clockTimer.Tick += ClockTimer_Tick;
             clockTimer.Start();
-        }
 
+            comboBoxBattery.SelectedIndexChanged += ComboBoxBattery_SelectedIndexChanged;
+            comboBoxBattery.DataSource = batteries;
+            comboBoxBattery.DisplayMember = "Name";
+        }
+        
+        private void ComboBoxBattery_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedBattery = (Battery)comboBoxBattery.SelectedItem;
+            UpdateBatteryRegisters();
+            UpdateMaxPowerCapacity();
+        }
+        private void numericUpDownBatteryBlocks_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateBatteryBlockCount((int)numericUpDownBatteryBlocks.Value);
+            UpdateMaxPowerCapacity();
+
+        }
+       
         private void ClockTimer_Tick(object sender, EventArgs e)
         {
             currentTime = currentTime.AddSeconds(1); // Increment current time by one second
             labelCurrentTime.Text = currentTime.ToString("HH:mm:ss"); // Update time label
+            UpdateRegisterDisplay();
+            UpdateBatteryInfo();
+            ChрeckRegisterOverLimitError();
+            ShowError();
+        }
+
+        private void UpdateRegisterDisplay()
+        {
+            listBoxBatteryInfo.Items.Clear(); // Очищення попереднього тексту
+            foreach (var register in registers)
+            {
+                listBoxBatteryInfo.Items.Add($"{register.Description}: {register.Value} {register.Unit}");
+            }
         }
 
         private void ComboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
@@ -268,6 +343,76 @@ namespace Invertor_sim
             else
             {
                 MessageBox.Show("Invalid time format. Please enter in HH:mm:ss format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SetBattaryPower(int value)
+        {
+            registers[9].Value = value;
+        }
+
+        //register changes
+        private void UpdateBatteryInfo()
+        {
+            registers[0].Value = registers[9].Value / registers[3].Value/ registers[2].Value;
+            registers[1].Value = (registers[0].Value - registers[5].Value) / (registers[4].Value - registers[5].Value) * 100; // % заряду батареї
+            
+
+        }
+        private void UpdateBatteryBlockCount(int blockCount)
+        {
+            registers[2].Value = blockCount;
+
+        }
+        private void UpdateMaxPowerCapacity()
+        {
+            registers[8].Value = registers[4].Value * registers[2].Value * registers[3].Value; // Макс запас енергії
+            registers[9].MaxValue = registers[8].Value;
+        }
+        private void UpdateBatteryRegisters()
+        {
+            registers[3].Value = selectedBattery.NominalCapacity; // Ємність
+            registers[4].Value = selectedBattery.MaxVoltage; // Максимальний вольтаж
+            registers[5].Value = selectedBattery.MinVoltage; // Мінімальний вольтаж
+            registers[6].Value = selectedBattery.NominalChargeDischargeCurrent; // Ток заряду/розряду
+            registers[7].Value = selectedBattery.NominalChargeDischargeCurrent;
+            
+
+        }
+        private void ShowError()
+        {
+            if (registers[11].Value != 0)
+            {
+                var error = registers[(int)registers[11].Value];
+                var errorRegis = registers[(int)error.Value];
+                label_Error.Text = "Error " + error.Description + " for " + errorRegis.Description;
+            }
+        }
+        private void ChрeckRegisterOverLimitError()
+        {
+            foreach (var register in registers)
+            {
+                if (register.Value > register.MaxValue)
+                {
+                    register.Value = register.MaxValue;
+                    registers[10].Value = register.RegisterNumber;//Привязать к RegisterNumber
+                    registers[11].Value = registers[10].RegisterNumber;//поиск ошибки OverLimit, если есть, добавление в регистр ошибок, регистр RegisterOverLimitError
+                    return;
+                }
+            }
+
+        }
+        private void BatteryPower_Click(object sender, EventArgs e)
+        {
+            // Проверка корректности введённого значения
+            if (int.TryParse(battaryPowerInput.Text, out int batteryPower))
+            {
+                // Если значение корректное, устанавливаем мощность батареи
+                SetBattaryPower(batteryPower);
+            }
+            else
+            {
+                // Если ввод некорректен, выводим сообщение об ошибке
+                MessageBox.Show("Please enter a valid number for battery power.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
